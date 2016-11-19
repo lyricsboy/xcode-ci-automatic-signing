d6ffc3f123b4ec9109a8ad4e181737120986002b
Brian Hardy <lyricsboy@users.noreply.github.com>
2016-11-19 18:06:52 -0500

    Initial version with automatic signing.

---
+++ AutomaticSigningCore/AutomaticSigningCore.h
@@ -0,0 +1,19 @@
+//
+//  AutomaticSigningCore.h
+//  AutomaticSigningCore
+//
+//  Created by Brian Hardy on 11/19/16.
+//  Copyright © 2016 Ponystyle Industries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AutomaticSigningCore.
+FOUNDATION_EXPORT double AutomaticSigningCoreVersionNumber;
+
+//! Project version string for AutomaticSigningCore.
+FOUNDATION_EXPORT const unsigned char AutomaticSigningCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AutomaticSigningCore/PublicHeader.h>
+
+
---
+++ AutomaticSigningCore/Test.swift
@@ -0,0 +1,13 @@
+//
+//  Test.swift
+//  AutomaticSigning
+//
+//  Created by Brian Hardy on 11/19/16.
+//  Copyright © 2016 Ponystyle Industries. All rights reserved.
+//
+
+import Foundation
+
+public func stringFromFramework() -> String {
+    return "Hello from the framework."
+}
