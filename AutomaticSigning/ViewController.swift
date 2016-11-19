499ffc31029a78724865809d61ef72ec340a3413
Brian Hardy <lyricsboy@users.noreply.github.com>
2016-11-19 17:31:23 -0500

    Initial Commit

---
+++ AutomaticSigning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutomaticSigning
+//
+//  Created by Brian Hardy on 11/19/16.
+//  Copyright © 2016 Ponystyle Industries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
