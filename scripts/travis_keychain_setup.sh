58c375af93c4770388db3feba5f123c828229a43
Brian Hardy <lyricsboy@users.noreply.github.com>
2016-11-20 16:02:16 -0500

    Code signing with cert

---
+++ scripts/travis_keychain_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# via: https://docs.travis-ci.com/user/common-build-problems/#Mac%3A-Code-Signing-Errors
+
+KEYCHAIN=travis.keychain
+security create-keychain -p travis $KEYCHAIN
+# Make the keychain the default so identities are found
+security default-keychain -s $KEYCHAIN
+# Unlock the keychain
+security unlock-keychain -p travis $KEYCHAIN
+# Set keychain locking timeout to 3600 seconds
+security set-keychain-settings -t 3600 -u $KEYCHAIN
