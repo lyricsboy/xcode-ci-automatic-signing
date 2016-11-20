86a4785937811a97f87e9158e5617f589654bbd3
Brian Hardy <lyricsboy@users.noreply.github.com>
2016-11-20 16:07:05 -0500

    Keychain location sanity

--- scripts/travis_keychain_setup.sh
+++ scripts/travis_keychain_setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # via: https://docs.travis-ci.com/user/common-build-problems/#Mac%3A-Code-Signing-Errors
 
-KEYCHAIN=travis.keychain
+KEYCHAIN=$TRAVIS_BUILD_DIR/travis.keychain
+echo "Setting up keychain at $KEYCHAIN"
 security create-keychain -p travis $KEYCHAIN
 # Make the keychain the default so identities are found
 security default-keychain -s $KEYCHAIN
